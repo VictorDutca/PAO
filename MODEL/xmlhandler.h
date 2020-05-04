788c7ba6bfc9bdf43a604b891afd2622e06b0652
Victor <victor.dutca@studenti.unipd.it>
2020-05-04 19:46:29 +0200

    Da finire XMLhandler_cpp

--- MODEL/xmlhandler.h
+++ MODEL/xmlhandler.h
@@ -4,14 +4,25 @@
 #include <QJsonDocument>
 #include <QXmlStreamReader>
 #include <QXmlStreamWriter>
+#include "lista.h"
+#include "workout.h"
+#include "nuoto.h"
+#include "corsa.h"
+#include "ciclismo.h"
+#include "triathlon.h"
 
-class XMLHandler
-{
-public:
-    XMLHandler();
+class XMLHandler {
+
 private:
     QXmlStreamReader XMLRead;
     QXmlStreamWriter XMLWrite;
+
+public:
+    XMLHandler();
+    void FileReader(QString);
+    Workout* WorkoutExplode(QString);
+    void FileWriter();
+
 };
 
 #endif // XMLHANDLER_H
